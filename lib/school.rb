f74a61f2055a9bdd4b62dabae4e76651414acbdb
cep104 <candicelizabethpeters@gmail.com>
2021-03-17 23:01:22 -0500

    commit tree 79a9075f7daad56416d126ac39da721de84c0540

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,31 @@
-require 'pry'
+require "pry"
 class School
+  attr_accessor :school, :roster
 
-    attr_accessor :name, :roster
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
 
-    def initialize(name)
-        @name = name
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+# binding.pry
+  end
 
-        @roster = {}
-    
-    
-    end
+  def grade(grade)
+    binding.pry
+    @roster[grade]
+  end
 
+  def sort
+    student_hash = {}
+    @roster.each do |grade, students|
+  #  binding.pry
+         student_hash[grade] = students.sort
+  end
+  student_hash
+  end
+  # hash["new_key"] << "new_value_for_value_array"
+  #{10=>["AC Slater"]}
 end
